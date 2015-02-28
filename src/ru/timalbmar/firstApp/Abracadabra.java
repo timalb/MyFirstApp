0091a5064752f88f511b5e95ad96b92adeb96d00
Albina Makarova <timalbmar@gmail.com>
2015-03-01 00:43:00 +0300

    my first commit

---
+++ src/ru/timalbmar/firstApp/Abracadabra.java
@@ -0,0 +1,10 @@
+package ru.timalbmar.firstApp;
+
+/**
+ * Created by блотр on 28.02.2015.
+ */
+public class Abracadabra {
+    public static void main(String[] args) {
+        System.out.println("Lol");
+    }
+}
